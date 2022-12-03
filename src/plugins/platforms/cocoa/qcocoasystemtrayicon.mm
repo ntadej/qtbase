f50adf7176da4979e159f264adbd0174246629ab
Tadej Novak <tadej@tano.si>
2022-12-03 20:02:17 +0100

    cocoa changes
    Change-Id: Idc78f8f439f667ba05d6e1df3ba308088a37f50c

--- src/plugins/platforms/cocoa/qcocoasystemtrayicon.mm
+++ src/plugins/platforms/cocoa/qcocoasystemtrayicon.mm
@@ -60,7 +60,8 @@ QT_BEGIN_NAMESPACE
 
 void QCocoaSystemTrayIcon::init()
 {
-    m_statusItem = [[NSStatusBar.systemStatusBar statusItemWithLength:NSSquareStatusItemLength] retain];
+    m_statusItem =
+            [[NSStatusBar.systemStatusBar statusItemWithLength:NSVariableStatusItemLength] retain];
 
     m_delegate = [[QStatusItemDelegate alloc] initWithSysTray:this];
 
@@ -68,6 +69,7 @@ void QCocoaSystemTrayIcon::init()
     // we fall back to the item's button to detect activation.
     m_statusItem.button.target = m_delegate;
     m_statusItem.button.action = @selector(statusItemClicked);
+    m_statusItem.button.imagePosition = NSImageRight;
     [m_statusItem.button sendActionOn:NSEventMaskLeftMouseDown | NSEventMaskRightMouseDown | NSEventMaskOtherMouseDown];
 }
 
@@ -115,7 +117,7 @@ void QCocoaSystemTrayIcon::updateIcon(const QIcon &icon)
     // (device independent pixels). The menu height on past and
     // current OS X versions is 22 points. Provide some future-proofing
     // by deriving the icon height from the menu height.
-    const int padding = 4;
+    const int padding = 2;
     const int menuHeight = NSStatusBar.systemStatusBar.thickness;
     const int maxImageHeight = menuHeight - padding;
 
@@ -199,7 +201,7 @@ void QCocoaSystemTrayIcon::updateToolTip(const QString &toolTip)
     if (!m_statusItem)
         return;
 
-    m_statusItem.button.toolTip = toolTip.toNSString();
+    m_statusItem.button.title = toolTip.toNSString();
 }
 
 bool QCocoaSystemTrayIcon::isSystemTrayAvailable() const
